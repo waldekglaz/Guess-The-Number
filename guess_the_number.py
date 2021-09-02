0f1801473e98ee6879c08737da7f643add3abfe5
waldekglaz <waldekglaz@gmail.com>
2021-09-02 22:18:18 +0100

    guess_the_nuber_v2

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,18 +2,16 @@ import random
 # random number
 number = random.randint(0, 100)
 guess = ""
-number_of_guesses = 0
+number_of_guesses = 1
 # loop over till guess == number
 while guess != str(number):
 
     guess = input("Guess the number between 0 and 100: ")
 
     if int(guess) > number:
-        number_of_guesses += 1
         print(
             f"secret number is lower than {guess}. Number of guesses: {number_of_guesses}")
     elif int(guess) < number:
-        number_of_guesses += 1
         print(
             f"The secret number is bigger than {guess}. Number of guesses: {number_of_guesses}")
     else:
@@ -21,3 +19,4 @@ while guess != str(number):
         print(
             f"Congratulations! The secret number was {number}. You guessed it in {number_of_guesses + 1} guesses")
         print("*")
+    number_of_guesses += 1
---
+++ guess_the_number_v2.py
@@ -0,0 +1,25 @@
+import random
+# random number
+number = random.randint(0, 100)
+guess = ""
+number_of_guesses_left = 5
+# loop over till guess == number or number_of_guesses_left
+while guess != str(number) or number_of_guesses_left <= 0:
+
+    guess = input("Guess the number between 0 and 100: ")
+
+    if guess > str(number):
+        print(
+            f"secret number is lower than {guess}. Number of guesses left: {number_of_guesses_left}")
+    elif guess < str(number):
+        print(
+            f"The secret number is bigger than {guess}. Number of guesses left: {number_of_guesses_left}")
+    else:
+        print("*")
+        print(
+            f"Congratulations! The secret number was {number}. You guessed the number is {5+1 - number_of_guesses_left} guesses")
+        print("*")
+    if number_of_guesses_left == 0:
+        print(f"You lose! The secret number was {number}")
+        break
+    number_of_guesses_left -= 1

618b8fe72fadfdaf38973d15ad60635a980696d6
waldekglaz <waldekglaz@gmail.com>
2021-09-29 17:28:43 +0100

    v3 completed

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,22 +1,52 @@
 import random
-# random number
-number = random.randint(0, 100)
-guess = ""
-number_of_guesses = 1
-# loop over till guess == number
-while guess != str(number):
 
-    guess = input("Guess the number between 0 and 100: ")
+from time import sleep
 
-    if int(guess) > number:
-        print(
-            f"secret number is lower than {guess}. Number of guesses: {number_of_guesses}")
-    elif int(guess) < number:
-        print(
-            f"The secret number is bigger than {guess}. Number of guesses: {number_of_guesses}")
+# create the random secret number
+
+
+def generate_the_number(min, max):
+    number = random.randint(min, max)
+    return number
+
+
+def game(number):
+    guess = ""
+    number_of_guesses_left = 12
+    number_of_guesses = 0
+
+    while not(guess == number) or (number_of_guesses_left == 0):
+        number_of_guesses_left -= 1
+        number_of_guesses += 1
+        guess = input("Guess the number between 0 and 100: ")
+        if guess == "":
+            print("We need a number!")
+        if guess > str(number):
+            print(
+                f"The secret number is lower than {guess}. Number of guesses left: {number_of_guesses_left}")
+        elif guess < str(number):
+            print(
+                f"The secret number is bigger than {guess}. Number of guesses left: {number_of_guesses_left}")
+        else:
+
+            print("*" * 5)
+            print(
+                f"Congratulations {player}! The secret number was {number}. You guessed the number in {number_of_guesses} guesses")
+            print("*" * 5)
+            sleep(2)
+            break
+        if number_of_guesses_left == 0:
+            print(f"Game Over. You lose! The secret number was {str(number)}")
+            sleep(2)
+            break
+
+
+while(True):
+    start_game = input("Do you want to start a new game? (Y)es or (N)o? > ")
+    if start_game.lower() == "y":
+        player = input("What is your name? > ")
+        print("Welcome", player)
+        game(generate_the_number(0, 100))
     else:
-        print("*")
-        print(
-            f"Congratulations! The secret number was {number}. You guessed it in {number_of_guesses + 1} guesses")
-        print("*")
-    number_of_guesses += 1
+        print("Good Bye!")
+        break
